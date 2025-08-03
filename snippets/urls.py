26ec4605eae1eb71bb3b116aa4bd896fd0212098
aapl00 <aapl00@proton.me>
2025-08-03 12:37:33 -0400

    Add SnippetHighlight view and update url

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,10 +4,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
+    path('', views.api_root),
     path('users/', views.UserList.as_view()), #type: ignore
     path('users/<int:pk>/', views.UserDetail.as_view()), #type: ignore
     path('snippets/', views.SnippetList.as_view()), #type: ignore
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()), #type: ignore
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()), #type: ignore
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
