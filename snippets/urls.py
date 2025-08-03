65b668968087a9e57bda6787979776511bd7287e
aapl00 <aapl00@proton.me>
2025-08-03 12:52:16 -0400

    Add name to urlpatterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,11 +5,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.api_root),
-    path('users/', views.UserList.as_view()), #type: ignore
-    path('users/<int:pk>/', views.UserDetail.as_view()), #type: ignore
-    path('snippets/', views.SnippetList.as_view()), #type: ignore
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()), #type: ignore
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()), #type: ignore
+    path('users/', views.UserList.as_view(), name='user-list'), #type: ignore
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'), #type: ignore
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'), #type: ignore
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'), #type: ignore
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(),
+        name='snippet-highlight'), #type: ignore
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
