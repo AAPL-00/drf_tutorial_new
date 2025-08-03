a69e51d105a783ac241aac8efaf93c8710af3da4
aapl00 <aapl00@proton.me>
2025-08-03 14:59:37 -0400

    Change urlpatterns to router

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,41 +1,11 @@
-from django.urls import path
-from snippets.views import UserViewSet, SnippetViewSet, api_root
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
+from snippets import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
 
-
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list',
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve',
-})
-
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('users/', user_list, name='user-list'), #type: ignore
-    path('users/<int:pk>/', user_detail, name='user-detail'), #type: ignore
-    path('snippets/', snippet_list, name='snippet-list'), #type: ignore
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'), #type: ignore
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'), #type: ignore
-])
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = [
+    path("", include(router.urls)),
+]
