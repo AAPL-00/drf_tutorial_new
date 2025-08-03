62578f387b3f0180d5daca55c4e6c57b006bdb40
aapl00 <aapl00@proton.me>
2025-08-03 11:36:45 -0400

    Update urls with UserList and UserDetails views

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,6 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
+    path('users/', views.UserList.as_view()), #type: ignore
+    path('users/<int:pk>/', views.UserDetail.as_view()), #type: ignore
     path('snippets/', views.SnippetList.as_view()), #type: ignore
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()), #type: ignore
 ]
